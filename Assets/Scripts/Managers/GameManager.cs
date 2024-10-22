64bb5101bd7b506aa4f3eb5b45ca5c0bf7c869da
JL-40 <RJandAprostudioz@gmail.com>
2024-10-22 02:25:58 -0400

    Created Chess board and piece prefab

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager _Instance;
 
-    [SerializeField] List<Sprite> _AsteroidSprites;
+    [Range(1,2)]
+    [SerializeField] int _currentPlayerTurn = 1;
+
+    [SerializeField] GameObject _GameBoard;
+    [SerializeField] List<List<GameObject>> _GameBoardSquares = new List<List<GameObject>>();
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +22,12 @@ public class GameManager : MonoBehaviour
             Destroy(this);
         }
         _Instance = this;
+
+        if (_GameBoard == null)
+        {
+            _GameBoard = GameObject.Find("GameBoard");
+        }
+
     }
 
     // Update is called once per frame
